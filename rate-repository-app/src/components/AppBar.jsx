028752fad2bc6af88b32f596e8387acc75afaef7
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-09-12 13:11:49 -0400

    Add routing & signin skeleton

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,4 +1,5 @@
-import { View, StyleSheet, Pressable } from 'react-native';
+import { View, ScrollView, StyleSheet, Pressable } from 'react-native';
+import { Link } from "react-router-native";
 import Constants from 'expo-constants';
 import theme from '../theme';
 import Text from './Text';
@@ -7,29 +8,42 @@ const paddingValue = 16;
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: Constants.statusBarHeight * 2,
+    paddingTop: Constants.statusBarHeight * 1.25,
     paddingBottom: paddingValue,
-    paddingLeft: paddingValue,
+    paddingHorizontal: paddingValue,
+    marginBottom: 12,
     backgroundColor: theme.colors.primary,
     display: 'flex',
-    flexDirection: 'row'
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
-  // ...
+  tabText:{
+    paddingHorizontal: paddingValue,
+  }
 });
 
-const AppBarTab = ({children}) => {
-  return <Pressable>
-    <Text fontWeight="bold" fontSize="subheading" color="textLight">
-      {children} &nbsp;
-    </Text>
-  </Pressable>
+const AppBarTab = ({ to, children }) => {
+  return (
+    <View style={styles.tabText}>
+      <Pressable>
+        <Link to={to}>
+          <Text fontWeight="bold" fontSize="subheading" color="textLight">
+            {children}
+          </Text>
+        </Link>
+      </Pressable>
+    </View>
+  )
 }
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab>Repositories</AppBarTab>
-      <AppBarTab>Other</AppBarTab>
+      <ScrollView horizontal >
+        <AppBarTab to={'/'}>Repositories</AppBarTab>
+        <AppBarTab to={'/signin'}>Sign In</AppBarTab>
+        <AppBarTab to={'/'}>Placeholder</AppBarTab>
+      </ScrollView>
     </View>
   );
 };
--- rate-repository-app/src/components/Main.jsx
+++ rate-repository-app/src/components/Main.jsx
@@ -1,6 +1,8 @@
 import { StyleSheet, View } from 'react-native';
+import { Route, Routes, Navigate } from 'react-router-native';
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
+import SignIn from './SignIn';
 
 const styles = StyleSheet.create({
   container: {
@@ -12,8 +14,12 @@ const styles = StyleSheet.create({
 const Main = () => {
   return (
     <View style={styles.container}>
-      <AppBar/>
-      <RepositoryList/>
+      <AppBar />
+      <Routes>
+        <Route path="/" element={<RepositoryList />} exact />
+        <Route path="/signin" element={<SignIn />} exact />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </View>
   );
 };
