c1ea00b2431c7fba7f7396895fa28be5a18b9ed5
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-09-12 01:21:32 -0400

    Extract AppBarTab to new component

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Pressable } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../theme';
 import Text from './Text';
@@ -7,20 +7,31 @@ const paddingValue = 16;
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: Constants.statusBarHeight*2,
+    paddingTop: Constants.statusBarHeight * 2,
     paddingBottom: paddingValue,
     paddingLeft: paddingValue,
     backgroundColor: theme.colors.primary,
+    display: 'flex',
+    flexDirection: 'row'
   },
   // ...
 });
 
-const AppBar = () => {
-  return <View style={styles.container}>
+const AppBarTab = ({children}) => {
+  return <Pressable>
     <Text fontWeight="bold" fontSize="subheading" color="textLight">
-      Repositories
+      {children} &nbsp;
     </Text>
-  </View>;
+  </Pressable>
+}
+
+const AppBar = () => {
+  return (
+    <View style={styles.container}>
+      <AppBarTab>Repositories</AppBarTab>
+      <AppBarTab>Other</AppBarTab>
+    </View>
+  );
 };
 
 export default AppBar;
